9fc39e899ad309c6ecb8eb2e17336cf95bccccea
AymericROUVRAIS <aymeric.rouvrais314@gmail.com>
2025-03-11 08:31:31 +0100

    Updating file

---
+++ code_global.ino
@@ -0,0 +1,225 @@
+/*
+  Version avec Serial Monitor
+
+  Code TIPE :
+  - Mesure angle de l'aile
+  - Mesure intensité du courant
+  - Mesure vitesse
+
+  - Ecrire sur Carte SD les données la trame :
+    angle, vitesse1, vitesse2, puissance(Um*I)
+
+  PIN LAYOUT cf Fritzing
+  Les LEDs servent seulement a voir si un problème
+  est arrivé lors de la mise en marche/de l'expérience
+*/
+
+
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h> // Module pour traduire la trame NMEA
+#include <SD.h>
+#include <SPI.h> // SPI module necessaire pour communication avec la carte SD
+
+
+// Communication serial vers le GPS
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(8,9);
+// Variables pour comuniquer avec les fichiers de la carte SD
+File logFile;
+File jTxt;
+// Variable pour retrouver la correspondance des LEDs
+#define cardState   4 // RED LED    -> Allumé si problème d'écriture
+#define ifileState  3 // WHITE LED  -> Allumé si problème de lecture sur i.txt
+#define fileState   2 // YELLOW LED -> Allumé si problème de lecture sur log.txt
+
+// Creation des variables globales
+const int chipSelect = 10;
+float u,i,v1,v2;
+int j,a=0;
+
+
+
+// =================
+//      Setup
+// =================
+void setup() {
+  Serial.begin(9600);
+  gpsSerial.begin(9600);
+
+  // Définir les LEDs
+  pinMode(ifileState, OUTPUT);
+  pinMode(cardState, OUTPUT);
+  pinMode(fileState, OUTPUT);
+  // Initialisé les LEDs
+  digitalWrite(ifileState, LOW);
+  digitalWrite(cardState, LOW);
+  digitalWrite(fileState, LOW);
+
+  // Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    // Serial.println("initialization failed");
+    digitalWrite(cardState, HIGH);
+  }
+  // Serial.println("initialization done.");
+  
+  // Faire un nouveau fichier log
+  char fileName[20];
+  jTxt = SD.open("i.txt", FILE_WRITE);
+  int j = lireDerniereLigne().toInt();
+  jTxt.close();
+
+  sprintf(fileName,"log%d.txt",j); // modifie fileName à log{j}.txt
+  logFile = SD.open(fileName, FILE_WRITE);
+  if (logFile){
+    logFile.println(j);
+    logFile.close();
+  }
+  else {
+    digitalWrite(fileState, HIGH);
+  }
+
+}
+
+String lireDerniereLigne(){
+  logFile = SD.open("test.txt", FILE_READ);
+
+  if (logFile) {
+    String lastLine = "";    // variable pour la dernière ligne
+    String currentLine = ""; // variable pour lire chaque ligne
+
+    // lit le fichier ligne par ligne
+    while (logFile.available()) {
+      char c = logFile.read();
+
+      if (c == '\n') {
+        // On a atteint le bout d'une ligne
+        lastLine = currentLine; //  lastLine
+        currentLine = "";       // preparer pour la nouvelle ligne
+      } else {
+        // Ajoute le charactère à la ligne
+        currentLine += c;
+      }
+    }
+    // A la fin, lastLine = la derniere ligne du fichier
+    if (currentLine.length() > 0) {
+      lastLine = currentLine;
+    }
+
+    // Serial.print("Last line: ");
+    // Serial.println(lastLine);
+
+    logFile.close();
+
+    return lastLine;
+  
+  } else {
+    // Fichier ne s'ouvre pas
+    // Serial.println("Error opening file.");
+    digitalWrite(ifileState, HIGH);
+  }
+}
+
+float vitesseGPS(){
+  /* On prend 2 vitesse de l'avion:
+     - Une donné par le module gps
+     - Une calculé à la main
+     Pour avoir une vitesse plus précise
+  */
+
+  if (gps.encode(gpsSerial.read() )){
+    // v1 donné directement par le gps
+    if (gps.location.isValid()){
+      // Serial.println(gps.speed.kmph());
+      v1 = gps.speed.kmph(); // vitesse en km/h
+    }
+    // else {
+    //  Serial.println("error with gps data");
+    // }
+  
+    // Calcul de v2 :
+    t1 += gps.time.hour()*3600;
+    t1 += gps.time.minute()*60;
+    t1 += gps.time.second();
+    d1 = pow(gps.location.lat(),2)-pow(gps.location.lng(),2);
+    d1 = pow(d1,0.5);
+        delay(500); // en ms
+    t2 += gps.time.hour()*3600;
+    t2 += gps.time.minute()*60;
+    t2 += gps.time.second();
+    d2 = pow(gps.location.lat(),2)-pow(gps.location.lng(),2);
+    d2 = pow(d2,0.5);
+
+    v2 = (d1-d2)/(t1-t2);
+
+  return v1,v2;
+  }
+}
+
+File dataToSD(){
+  logFile = SD.open("log.txt", FILE_WRITE);
+  if (logFile) {
+    // Serial.print("Writing to log.txt...");
+    logFile.print(a); // Ecriture du fichier
+    logFile.print(", ");
+    logFile.print(i);
+    logFile.print(", ");
+    logFile.println(v1);
+    logFile.print(", ");
+    logFile.println(v2);
+
+    
+    logFile.close();
+    // Serial.println("done.");
+  } else {
+    // Si le fichier ne s'ouvre pas
+    // Serial.println("error opening log.txt");
+    digitalWrite(fileState, HIGH);
+  }
+
+  // Re-ouvre le fichier pour lire
+  logFile = SD.open("test.txt");
+  if (logFile) {
+    Serial.println("test.txt:");
+
+    // Lit le fichier jusqu'à la fin
+    while (logFile.available()) {
+      Serial.write(logFile.read());
+    }
+    logFile.close();
+  }
+  else {
+    // Serial.println("error opening log.txt");
+    digitalWrite(fileState, HIGH);
+  }
+}
+
+
+
+
+// =================
+//      Looop
+// =================
+void loop() {
+  // lecture tension capteur effet Hall
+  u = analogRead(A0);
+  // conversion en intensité du courant capté
+  i = 0.39*(u-512);
+
+  // le moteur est commandé en intensité
+  // Umot est constant : 14.8V pour batterie 4s
+  i = i*14.8;
+
+  // Serial.print("I : ");
+  // Serial.println(i);
+
+
+  // lecture de l'angle
+  a = analogRead(A1);
+  // conversion de byte en degrée
+  a = a*270/1023;
+
+  // lecture de la vitesse
+  v = vitesseGPS();
+
+  dataToSD();
+}
---
+++ post_process.py
@@ -0,0 +1,12 @@
+'''
+    Code TIPE :
+    - Récupère les données des log{i}.txt
+      des résultats expérimentaux donnés par
+      Arduino. Fichier de la forme:
+          angle, vitesse1,vitesse2, puissance
+    - Créer une base de données sous la forme:
+        {angle1 : [(vitesse1, vitesse2, puissance moteur)] }
+    - Cherche le meilleur angle pour une vitesse donnée
+
+
+'''
